a45bfabe104972be85cf75afa3cf369167ed4288
Avijit Chowdhury <abhichy30@gmail.com>
2022-07-22 22:49:21 +0600

    ml

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('F:\ML_project\Multiple Disease Prediction System/saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('./saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('F:\ML_project\Multiple Disease Prediction System/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('./saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('F:\ML_project\Multiple Disease Prediction System/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('./saved models/parkinsons_model.sav', 'rb'))
 
 
 
